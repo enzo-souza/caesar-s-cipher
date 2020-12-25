2a44a74f2b1ce82c135b1f95b0cfe93ae7412983
enzo-souza <enzocrimadesouza@gmail.com>
2020-12-25 18:48:47 -0300

    correcting some error
    now my code do not count " " (space)

--- cesar's cipher/identificador_de_chave.py
+++ cesar's cipher/identificador_de_chave.py
@@ -3,15 +3,18 @@ new_message = ''
 
 letter_percent=[]  
 
-for i in range(len(message)): #calculating of the percentage of the message's letters
+for i in range(len(message)): #calculation of the percentage of the message's letters
     letter_score = 0
 
-    if message[i]  not in new_message or message[i] != ' ':
-
+    if message[i]  not in new_message and message[i] != ' ':
+    
         for j in range(len(message)):
             if message[i] == message[j]:
                 letter_score += 1
-                new_message += message[i]
-        letter_percent.append([message[i],f'{(letter_score/len(message)):.2f}']) #put in a list (letter_percent) the percent of each letter in te messege
-
+                if message[i] not in new_message:
+                    new_message += message[i]
+                
+        letter_percent.append([message[i],f'{(letter_score/len(message)):.2f}']) #put in a list (letter_percent) the percent of each letter in the messege
+        print(new_message,'--',message[i])
 print(letter_percent)
+
---
+++ teste.py
@@ -0,0 +1,9 @@
+message = 'a a '
+
+A = "a a "
+
+if A not in message or message != " ":
+    print("ssdasd")
+
+else:
+    print('não')
