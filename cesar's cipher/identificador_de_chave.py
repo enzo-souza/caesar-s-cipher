30bf5167e426bf483c7f4a40520faf8534cbe84a
enzo-souza <enzocrimadesouza@gmail.com>
2020-12-25 17:32:09 -0300

    Update identificador_de_chave.py

--- cesar's cipher/identificador_de_chave.py
+++ cesar's cipher/identificador_de_chave.py
@@ -1,9 +1,9 @@
 message = 'enzo gabriel'
 new_message = ''
 
-letter_percent=[]
+letter_percent=[]  
 
-for i in range(len(message)):
+for i in range(len(message)): #calculating of the percentage of the message's letters
     letter_score = 0
 
     if message[i]  not in new_message or message[i] != ' ':
@@ -12,6 +12,6 @@ for i in range(len(message)):
             if message[i] == message[j]:
                 letter_score += 1
                 new_message += message[i]
-        letter_percent.append([message[i],f'{(letter_score/len(message)):.2f}'])
+        letter_percent.append([message[i],f'{(letter_score/len(message)):.2f}']) #put in a list (letter_percent) the percent of each letter in te messege
 
 print(letter_percent)
